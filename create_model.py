fixed large issue with leakage and confirmed that this is no longer a problem. also updated open decay function

create_model.py
@@ -9,6 +9,7 @@ import sys
 #potential update: have these be command line params
 full_flag = False
 save = True
+#name = '_simple'
 #create dataset
 train_packs,train_picks,test_packs,test_picks = create_dataset(full_dataset=full_flag,save_clusters=save)
 #initialize model with 249 cards and 15 archetypes
@@ -25,7 +26,7 @@ train_y = torch.flatten(train_picks[:,16:,:],start_dim=0,end_dim=1)
 #train the model
 train_loss = utils.train(rank_model,loss_function,optimizer,train_x,train_y,epochs=5)
 if save:
-	torch.save(rank_model,'Saved_Models/rank_model.pkl')
+	torch.save(rank_model,'Saved_Models/rank_model_final.pkl')
 #initialize drafting model with learned weights from rank model
 init_weights = rank_model.rank_matrix.clone().detach()
 #normalize the weights such that 1 is the largest initial weight
@@ -34,7 +35,7 @@ draft_model = DraftNet(smaller_init_weights)
 #add l2 regularization to avoid exploding weights
 #with regularization, also lower the learning rate and increase epochs
 #note: with this regularization there is no need for ceiling on pool bias.
-optimizer = torch.optim.Adam(draft_model.parameters(), lr=0.01,weight_decay=1e-5)
+optimizer = torch.optim.Adam(draft_model.parameters(), lr=0.1,weight_decay=1e-5)
 #flatten the drafts so that the algorithm only considers each pick
 #individually and remove archetype label to avoid leakage
 train_x = torch.flatten(train_packs,start_dim=0,end_dim=1)[:,1:]
@@ -64,9 +65,9 @@ if not full_flag:
 	plt.xlabel('Time (pick number)')
 	plt.ylabel('Accuracy')
 	plt.title('Avg Accuracy in Test Set')
-	plt.savefig('Output/accuracy_curve.png')
+	plt.savefig('Output/accuracy_curve_final.png')
 if save:
 	#save the model so it can be used to make decisions in a real draft
-	torch.save(losses,'Data/train_loss.pkl')
-	torch.save(draft_model,'Saved_Models/draft_model.pkl')
+	torch.save(losses,'Data/train_loss_final.pkl')
+	torch.save(draft_model,'Saved_Models/draft_model_final.pkl')
 

models.py
@@ -48,16 +48,19 @@ class DraftNet(torch.nn.Module):
                                         )
                                     )
         #vector to express opposition to bias (staying open)
-        #originally I did not enforce a decaying structure,
-        #    and the bot learned to raredraft. So I added this
-        #    decay via an inverted cumulative sum on these weights
+        #the initialization of 2 as open_base and 7 is purposeful.
+        #This is because cumprod(sigmoid(open_base)) * 7 - minimum has a value
+        # of tensor(1.0090) at Pack 2 Pick 1. I tested the algorithm without
+        # this initialization and it did learn the same curve, but I see no 
+        # reason to change the initialization and I wanted to explain the numbers.
         self.open_base = torch.nn.Parameter(torch.tensor(
-                                            torch.zeros(42),
+                                            [2.0 for x in range(42)],
                                             requires_grad=True,
                                             dtype=torch.float
                                         ))
+        self.lift = torch.nn.Parameter(torch.tensor(7.0,requires_grad=True))
         #placeholder for future versions. Currently doesn't update, but I want to explore that.
-        self.arch_bias = torch.nn.Parameter(torch.ones(self.n_archs,requires_grad=False))
+        self.arch_bias = torch.ones(self.n_archs,requires_grad=False)
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
     def forward(self,x):
@@ -72,7 +75,7 @@ class DraftNet(torch.nn.Module):
         #squash open_base numbers between 0 and 1
         open_base = self.sigmoid(self.open_base)
         #enforce decaying structure over time
-        open_decay = torch.flip(torch.cumsum(open_base,dim=0),dims=[0])
+        open_decay = torch.cumprod(open_base,dim=0) * self.relu(self.lift)
         #enforce this decay to go to zero
         open_decay = open_decay - open_decay.min()
         #get the proper open bias forr each pick

preprocessing.py
@@ -9,7 +9,7 @@ def add_clusters(features,drafts,n_archetypes,colors_only=False,save=True):
 	draft_pool_vectors = aggregate_drafts(drafts[:,:,1:],features,colors_only=colors_only)
 	kmeans = KMeans(n_clusters=n_archetypes).fit(draft_pool_vectors)
 	if save:
-		torch.save(kmeans,'Saved_Models/clusters.pkl')
+		torch.save(kmeans,'Saved_Models/clusters_final.pkl')
 	return kmeans
 def aggregate_drafts(drafts,features,colors_only=False):
 	"""
@@ -37,8 +37,8 @@ def create_drafts():
 	The actual code and pickled files are excluded from the repo due to NDA, but
 	this is the body replacing it in order to test that the pipeline works.
 	"""
-	draft_picks = torch.load('Data/dpicks.pkl')
-	draft_packs = torch.load('Data/dpacks.pkl')
+	draft_picks = torch.load('Data/draft_picks.pkl')
+	draft_packs = torch.load('Data/draft_packs.pkl')
 	return draft_packs,draft_picks
 
 def get_format_features():
@@ -83,8 +83,8 @@ def create_dataset(n_archetypes=15,full_dataset=False,save_clusters=True):
 	test_idx = list(set(range(size)) - set(train_idx))
 	if not full_dataset:
 		#store the train/test split for the current model
-		torch.save(test_idx,'Data/test_idx.pkl')
-		torch.save(train_idx,'Data/train_idx.pkl')
+		torch.save(test_idx,'Data/test_idx_final.pkl')
+		torch.save(train_idx,'Data/train_idx_final.pkl')
 	train_pack = draft_packs[train_idx,:,:]
 	train_pick = draft_picks[train_idx,:,:]
 	test_pack = draft_packs[test_idx,:,:]